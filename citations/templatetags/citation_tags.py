a1734df0af1d308924edcc4c5327fde9c8d84fed
Yasushi Masuda <whosaysni@gmail.com>
2015-05-23 12:31:42 +0900

    fixed typo.

--- citations/templatetags/citation_tags.py
+++ citations/templatetags/citation_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from django.utils.translation umport ugettext as _
+from django.utils.translation import ugettext as _
 
 from citations.models import Reference as R
 from citations.utilities import dc_alpha_counter
