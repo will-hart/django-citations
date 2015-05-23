3bb0156ef9c3dd0e008e2986be3c577558ea0ba5
Yasushi Masuda <whosaysni@gmail.com>
2015-05-23 12:43:21 +0900

    Field name markups.

--- citations/models.py
+++ citations/models.py
@@ -14,28 +14,28 @@ REFERENCE_TYPE_CHOICES = (
 
 
 class Reference(models.Model):
-    type = models.CharField(max_length=3, choices=REFERENCE_TYPE_CHOICES, default='BK')
-    slug = models.CharField(max_length=128, unique=True)
+    type = models.CharField(_('Type'), max_length=3, choices=REFERENCE_TYPE_CHOICES, default='BK')
+    slug = models.CharField(_('Slug'), max_length=128, unique=True)
 
-    author = models.CharField(max_length=512)
-    title = models.CharField(max_length=512)
-    year = models.IntegerField(default=2000)
+    author = models.CharField(_('Author'), max_length=512)
+    title = models.CharField(_('Title'), max_length=512)
+    year = models.IntegerField(_('Year'), default=2000)
 
-    series = models.CharField(max_length=512, blank=True, null=True)
-    volume = models.IntegerField(blank=True, null=True)
-    edition = models.IntegerField(blank=True, null=True)
+    series = models.CharField(_('Series'), max_length=512, blank=True, null=True)
+    volume = models.IntegerField(_('Volume'), blank=True, null=True)
+    edition = models.IntegerField(_('Edition'), blank=True, null=True)
 
-    isbn = models.CharField(max_length=17, blank=True, null=True)
-    url = models.URLField(blank=True, null=True)
+    isbn = models.CharField(_('ISBN'), max_length=17, blank=True, null=True)
+    url = models.URLField(_('URL'), blank=True, null=True)
 
-    publisher = models.CharField(max_length=128, blank=True, null=True)
-    place = models.CharField(max_length=128, blank=True, null=True)
+    publisher = models.CharField(_('Publisher'), max_length=128, blank=True, null=True)
+    place = models.CharField(_('Place'), max_length=128, blank=True, null=True)
 
-    abstract = models.TextField(blank=True, null=True)
-    comments = models.TextField(blank=True, null=True)
-    keywords = models.TextField(blank=True, null=True)
+    abstract = models.TextField(_('Abstract'), blank=True, null=True)
+    comments = models.TextField(_('Comments'), blank=True, null=True)
+    keywords = models.TextField(_('Keywords'), blank=True, null=True)
 
-    accessed = models.DateField(default=datetime.now(pytz.utc))
+    accessed = models.DateField(_('Accessed'), default=datetime.now(pytz.utc))
 
     class Meta:
         verbose_name = _('Reference')
